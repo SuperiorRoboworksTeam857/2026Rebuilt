04c036e5ce17d23a7e116eba5aadaf22741fefe5
857 programming <csdoig@mtu.edu>
2026-02-03 19:52:51 -0500

    added shooter run function

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,6 +10,8 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.Swerve.IntakeConstants;
+import frc.robot.Constants.Swerve.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
 
@@ -20,7 +22,7 @@ public class Shooter extends SubsystemBase {
   public Shooter() {
 
     SmartDashboard.putNumber("shooterMotorSpeed", 0);
-    
+
   }
 
   /**
@@ -38,7 +40,8 @@ public class Shooter extends SubsystemBase {
   }
 
   /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   * An example method querying a boolean state of the subsystem (for example, a
+   * digital sensor).
    *
    * @return value of some boolean subsystem state, such as a digital sensor.
    */
@@ -47,13 +50,17 @@ public class Shooter extends SubsystemBase {
     return false;
   }
 
+  public void runShooter(double speed) {
+    shooterMotor1.set(speed * ShooterConstants.shooterSpeedMultiplier);
+    shooterMotor2.set(speed * ShooterConstants.shooterSpeedMultiplier);
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     double shooterMotorPower = SmartDashboard.getNumber("shooterMotorSpeed", 0f);
     shooterMotor1.set(shooterMotorPower);
     shooterMotor2.set(shooterMotorPower);
-    
 
     // shooterMotor1.setVoltage(shooterMotorPower);
     // shooterMotor2.setVoltage(shooterMotorPower);
