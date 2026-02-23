2f6a4c0912d342c1709e302d7e51b873de7cc1ed
857 programming <csdoig@mtu.edu>
2026-02-23 18:41:08 -0500

    angle offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -90,6 +90,7 @@ public final class Constants {
       public static final int driveMotorID = 12;
       public static final int angleMotorID = 11;
       public static final int canCoderID = 1;
+      // TODO: need to refind angle offsets
       public static final Rotation2d angleOffset = Rotation2d.fromDegrees(84.375);
       public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID,
           canCoderID, angleOffset);
