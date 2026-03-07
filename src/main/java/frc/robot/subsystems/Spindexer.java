c8ed31809bfd55e931de32a1ba66bc9ad760ada6
857 programming <csdoig@mtu.edu>
2026-03-07 12:01:52 -0500

    simplify spindexer code to just a percent speed

--- src/main/java/frc/robot/subsystems/Spindexer.java
+++ src/main/java/frc/robot/subsystems/Spindexer.java
@@ -4,9 +4,7 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
@@ -14,7 +12,6 @@ import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -23,59 +20,29 @@ public class Spindexer extends SubsystemBase {
   // one spindexerMotor in the middle that spins to bring the fuel cells around
   private SparkFlex spindexerMotor = new SparkFlex(Constants.SpindexerConstants.spindexerWheel, MotorType.kBrushless);
   private SparkFlexConfig spindexerConfig = new SparkFlexConfig(); // to handle the PID loop of the middle loop
-  private SparkClosedLoopController spindexerController = spindexerMotor.getClosedLoopController();
-  private double targetSpindexerSpeed = 0;
+  private double targetSpindexerSpeed = 0.4;
 
   public Spindexer() {
-    SmartDashboard.putNumber("spindexerMotorSpeed", 0);
-
-    // setup PID parameters
-    spindexerConfig.closedLoop
-      .p(Constants.SpindexerConstants.spindexerKP)
-      .i(Constants.SpindexerConstants.spindexerKI)
-      .d(Constants.SpindexerConstants.spindexerKD);
-
     spindexerConfig.idleMode(IdleMode.kBrake);
 
     spindexerMotor.configure(spindexerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  }
-
-  public void setSpindexerVelocity(double velocity){
-    spindexerController.setSetpoint(velocity, ControlType.kVelocity);
   }
   
   public void runSpindexer(double speed) {
     spindexerMotor.set(speed * Constants.SpindexerConstants.spindexerSpeedMultiplier);
   }
-  public void powerSpindexer(double speed) {
-    if(Constants.SpindexerConstants.usePID)
-      setSpindexerVelocity(speed);
-    else
-      runSpindexer(speed);
-  }
   public void startSpindexer() {
-    powerSpindexer(targetSpindexerSpeed);
-
+    runSpindexer(targetSpindexerSpeed);
   }
   public void stopSpindexer() {
-    powerSpindexer(0);
+    runSpindexer(0);
   }
   public void reverseSpindexer() {
-    powerSpindexer(-targetSpindexerSpeed);
+    runSpindexer(-targetSpindexerSpeed);
   }
 
   @Override
   public void periodic() {
-    // grab from the dashboard the speed for the spindexer and set it
-    // default 0 so it doesn't run when we don't want it to
-    double spindexerMotorPower = SmartDashboard.getNumber("spindexerMotorSpeed", 0f);
-    targetSpindexerSpeed = spindexerMotorPower;
-    // choose between PID control and just setting the power to the motor based on the constant
-    // if(Constants.SpindexerConstants.usePID)
-    //   setSpindexerVelocity(spindexerMotorPower);
-    // else
-    //   runSpindexer(spindexerMotorPower);
-
     SmartDashboard.putNumber("spindexer motor actual RPM", spindexerMotor.getEncoder().getVelocity());
   }
 
