d377679c860c5977f9dc30b30cdc7ddd4e41935e
857 programming <csdoig@mtu.edu>
2026-04-04 15:10:26 -0400

    slow down agitator a bit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -118,7 +118,7 @@ public final class Constants {
     public static final int spindexerWheel = 24; // will spin in the center
     public static final int spindexerAgitator = 30; //top roller above spindexer
     public static final double spindexerSpeedMultiplier = 1; // basic speed multiplier
-    public static final double spindexerAgitatorMultiplier = 1;
+    public static final double spindexerAgitatorMultiplier = .7;
   }
 
   // constants for the Feeder
